116f1aabbc827d0d1e2dbf8a9edc4059a4806ed1
Jared Elliott <bootcamp@jaredclt.com>
2024-01-11 21:50:33 -0500

    Added questions

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Including: Inquirer, FS, generateMarkdown.js
 const inq = require('inquirer');
 const fs = require('fs');
-const markdown = require('./utils/generateMarkdown.js');
+const mark = require('./utils/generateMarkdown.js');
 
 function logme(message, label) {
     //logs to the console.. just making things shorter
     console.log(label, message)
 }
 
-function writeToFile(fileName, data, ) {
+function writeToFile(fileName, data) {
     // Writes to fileName, path provided
     fileName ? fs.writeFile(fileName, data, err => {
         if (err) { logme(err) }}) : logme(`${fileName} is not valid.`); 
@@ -21,24 +21,85 @@ const questions = [
         message: 'What is your GitHub Username?',
         name: 'ghUser',
         validate: function (test) { 
-            if(test.length < 1) { return 'Must be a valid username.'; } return true;
+            if(test.trim().length < 1) { return 'Must be a valid username.'; } return true;
         }
     },
     {
         type: 'input',
         message: 'What is your Project Name?',
-        name: 'projectName'
+        name: 'project',
+        validate: function (test) {
+            if(test.trim().length < 1) { return 'Must be a valid project name.'; } return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'How would you describe this project? (minimum 5 chars)',
+        name: 'description',
+        validate: function (test) {
+            if(test.length < 5) { return 'Must be a valid description.'; } return true;
+        }
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your project:',
+        name: 'license',
+        choices: ['MIT', 'GPLv3', 'Open Software License 3.0', 'The Unlicense', 'None']
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+        validate: function (test) {
+            if(test.length < 1) { return 'Email address is not valid.'; } 
+            else if(!test.includes('@')) { return 'Email address is not valid.'; }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'How is this installed?',
+        name: 'installation',
+        validate: function (test) {
+            if(test.length < 1) { return 'Must be a valid input.'; } return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'How is this project used?',
+        name: 'usage',
+        validate: function (test) {
+            if(test.length < 1) { return 'Must be a valid input.'; } return true;
+        }
+    }, 
+    {
+        type: 'input',
+        message: 'Did you have any contributors?',
+        name: 'contributors'
+    },  
+    {
+        type: 'input',
+        message: 'What were your test cases?',
+        name: 'test',
+        validate: function (test) {
+            if(test.length < 1) { return 'Must be a valid test case.'; } return true;
+        }
+    },  
+    {
+        type: 'input',
+        message: 'How should readers contact you?',
+        name: 'contact',
+        validate: function (test) {
+            if(test.length < 1) { return 'Must be a valid input.'; } return true;
+        }
     }
 ]
 
-
-//async function to allow for await
-async function init() {
-    const usrResponse = await inq.prompt(questions);
-    logme(usrResponse, "Response:");
-
-    //Let's write our file
-    writeToFile(`./output/README.md`, removeMe);
+function init() {
+    inq.prompt(questions).then((response) => {
+        const goodData = mark.generateMarkdown(response);
+        writeToFile(`./output/README.md`, goodData);
+    })
 }
 
 // Function call to initialize app
