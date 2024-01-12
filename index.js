8973afee840ad881be85d1428ada4fbebf3983aa
Jared Elliott <bootcamp@jaredclt.com>
2024-01-11 19:30:39 -0500

    Added questions into the array. Changed init function to an async to allow for await.

--- index.js
+++ index.js
@@ -3,23 +3,39 @@ const inq = require('inquirer');
 const fs = require('fs');
 const markdown = require('./utils/generateMarkdown.js');
 
-function logit(message, label) {
+function logme(message, label) {
+    //logs to the console.. just making things shorter
     console.log(label, message)
 }
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
 function writeToFile(fileName, data, ) {
+    // Writes to fileName, path provided
     fileName ? fs.writeFile(fileName, data, err => {
-        if (err) { logit(err) }}) : logit(`${fileName} is not valid.`); 
+        if (err) { logme(err) }}) : logme(`${fileName} is not valid.`); 
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    //Fake variable
-    const removeMe = 'test data.'
+const questions = [
+    //creating questions for the inquirer prompt
+    {
+        type: 'input',
+        message: 'What is your GitHub Username?',
+        name: 'ghUser',
+        validate: function (test) { 
+            if(test.length < 1) { return 'Must be a valid username.'; } return true;
+        }
+    },
+    {
+        type: 'input',
+        message: 'What is your Project Name?',
+        name: 'projectName'
+    }
+]
+
+
+//async function to allow for await
+async function init() {
+    const usrResponse = await inq.prompt(questions);
+    logme(usrResponse, "Response:");
 
     //Let's write our file
     writeToFile(`./output/README.md`, removeMe);
